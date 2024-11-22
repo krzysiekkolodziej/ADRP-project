abafb430dacc72a45358e9fd1ac83a00af16558d
kubastudent121 <dudajakub789@o2.pl>
2024-11-22 21:00:29 +0100

    Krzys sprawdz jeszcze

--- projekt.py
+++ projekt.py
@@ -1,10 +1,16 @@
 import pandas as pd
 
 # Załadowanie danych
-df = pd.read_csv('winequality-red.csv')
+df = pd.read_csv('cleaned_data.csv')
 
-# Sprawdzanie, czy są duplikaty
-duplikaty = df.duplicated()
+# Wybór tylko kolumn liczbowych
+numeric_columns = df.select_dtypes(include=['float64', 'int64']).columns
 
-# Wyświetlenie wierszy, które są duplikatami
-print(df[duplikaty])
+# Normalizacja Min-Max za pomocą Pandas
+df[numeric_columns] = df[numeric_columns].apply(lambda x: (x - x.min()) / (x.max() - x.min()))
+
+# Sprawdzenie wyników
+print(df.describe())
+
+# Zapisanie znormalizowanych danych do nowego pliku CSV
+df.to_csv('normalized_cleaned_data.csv', index=False)
